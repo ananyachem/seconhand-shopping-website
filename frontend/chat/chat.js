027966917ece0345fd301a0e8bf7f775f224f42a
ananya 3 <108514240+ananyachem@users.noreply.github.com>
2023-12-05 23:47:09 +0530

    Add files via upload

---
+++ frontend/chat/chat.js
@@ -0,0 +1,126 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+const BOT_IMG = "bot.png";
+const PERSON_IMG = "user.png";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Customer";
+
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"], //0
+  ["how are you", "how is life", "how are things"], //1
+  ["what do you sell"], //2
+  ["i want to buy"], //3
+  ["scrunchies"], //4
+  ["wallets"], //5
+  ["jewellery"], //6
+  ["clothes"], //7
+  ["bath products"], //8
+  ["desserts"], //9
+]
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"], //0
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ], //1
+  ["Scrunchies, Desserts, Wallets, Jewellery, Clothes, Bath products"], //2
+  ["what do you want to buy"], //3
+  ["@handmadescrunchie_"], //4
+  ["@haveitbagit", "@thetrendmakers", "@bombaythreads"], //5
+  ["@cutiefy", "@unimaginableprices"], //6
+  ["@femella", "@shopxanomaly", "@artisanluxe", "@rozedo_jaipur"], //7
+  ["@soapaffairs", "@foamandfizzsoaps"], //8
+  ["@thewhiskcorner", "@thechibicakeshop", "@sugarfanciesbypooja"], //9
+];
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+const robot = ["How do you do, fellow human", "I am not a bot"];
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+  msgerInput.value = "";
+  addChat(PERSON_NAME, PERSON_IMG, "right", msgText);
+  output(msgText);
+});
+function output(input) {
+  let product;
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+  text = text
+    .replace(/ a /g, " ")  
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
+  if (compare(prompts, replies, text)) {
+    product = compare(prompts, replies, text);
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(robot|bot|robo)/gi)) {
+    product = robot[Math.floor(Math.random() * robot.length)];
+  } else {
+    product = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+  const delay = input.split(" ").length * 100;
+  setTimeout(() => {
+    addChat(BOT_NAME, BOT_IMG, "left", product);
+  }, delay);
+}
+function compare(promptsArray, repliesArray, string) {
+  let reply;
+  let replyFound = false;
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
+        reply = replies[Math.floor(Math.random() * replies.length)];
+        replyFound = true;
+        break;
+      }
+    }
+    if (replyFound) {
+      break;
+    }
+  }
+  return reply;
+}
+function addChat(name, img, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
